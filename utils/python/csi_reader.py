a7c0e045f7268c4ebbcffface7983428c4188694
Thomas Weigel <thomas.weigel@outlook.de>
2020-12-02 12:02:28 +0100

    python - CsiEntry

--- utils/python/csi_reader.py
+++ utils/python/csi_reader.py
@@ -3,15 +3,16 @@ BANDWIDTH ={ # MHz to size of CSI
     "40": 128,
     "80": 256
 }
-
+import matplotlib.pyplot as plt
 FRAME_IP_HEADER_SIZE = 42
 UDP_HEADER_SIZE = 18 # bytes without csi flow
 from pylibpcap.pcap import rpcap as cap
 import numpy as np
 import json
 import plot
-FILE = "./example.pcap"
-#FILE = "capture_output.pcap"
+
+#FILE = "./example.pcap"
+FILE = "capture_output.pcap"
 import os
 #import CsiEntry
 CHIPS = ["bcm43455c0"]
@@ -38,16 +39,18 @@ def extract_csi(udp_payload):
     udp_dict["src_mac"]                 = udp_payload[4:10].hex()
     udp_dict["seq_nr"]                  = udp_payload[10:11].hex()
     udp_dict["spartial_stream_core_nr"] = udp_payload[12:13].hex()
+    udp_dict["rx"]                      = 1 # TODO merge multible packages
     udp_dict["chanspec"]                = udp_payload[14:15].hex()
     udp_dict["chip_version"]            = udp_payload[16:17].hex()
     print(udp_dict)
-    udp_dict["csi_matrix"]              = format_csi(udp_payload[UDP_HEADER_SIZE:],CHIPS[0]) 
-    udp_dict["csi_matrix_size"]         = len(udp_dict["csi_matrix"])
+    udp_dict["csi"]              = format_csi(udp_payload[UDP_HEADER_SIZE:],CHIPS[0]) 
+    udp_dict["csi_matrix_size"]         = len(udp_dict["csi"])
     #print(udp_payload)
     #print(pkt)
     #print(udp_dict)
     #print(udp_payload[4:10].hex())
-    return udp_dict
+    entry = plot.CsiEntry.from_dict(udp_dict)
+    return entry
 if __name__ == "__main__":
     if not os.path.isfile(FILE):
         raise Exception(f"No file found {FILE}")
@@ -67,14 +70,16 @@ if __name__ == "__main__":
         elif payload_len == (UDP_HEADER_SIZE + 4*BANDWIDTH["80"]):
             pass
         else:
-            raise Exception(f"invalid udp_payload length :{payload_len}")
-        csi_array = extract_csi(udp_payload)
-        entry = plot.CsiEntry().csi = csi_array
-        csi_entrys.append(entry)
+            print("scipped pacakges")
+            continue
+            #raise Exception(f"invalid udp_payload length :{payload_len}")
+        csi_entry = extract_csi(udp_payload)
+        csi_entrys.append(csi_entry)
 
-
-    for entry in csi_entrys:
-        plot.plot_ampli(entry)
+    plt.subplot()
+    for entry in csi_entrys[:10]:
+        plot.plot_ampli(entry) 
+    plt.show()
 
 
 
--- utils/python/plot.py
+++ utils/python/plot.py
@@ -1,102 +1,53 @@
-
-import matplotlib
+import matplotlib.pyplot as plt
 import json
-
+import math
 
 
 class CsiEntry( json.JSONDecoder):
     """docstring for CsiEntry"""
     def __init__(self):
         super(CsiEntry, self).__init__()
-        self.correct = True
-        self.code = None
-        self.bfee_count = None
-        self.Nrx = None
-        self.Ntx = None
-        self.rssiA = None
-        self.rssiB = None
-        self.rssiC = None
-        self.noise = None
-        self.agc = None
-        self.antenna_sel = None
-        self.length = None
-        self.rate = None
-        self.rssiA_db = None
-        self.rssiB_db = None
-        self.rssiC_db = None
-        self.csi = None
-        self.perm = None
-        self.csi_pwr = None
-        self.rssi_pwr_db = None
-        self.rssi_pwr = None
-        self.scale = None
-        self.noise_db = None
-        self.quant_error_pwr = None
-        self.total_noise_pwr = None
+        self.Nrx    = None
+        self.Ntx    = None
+        self.rss    = None
+        self.rssA   = None
+        self.rssB   = None
+        self.rssC   = None
+        self.noise  = None
+        self.rss    = None
+        self.snr    = None
+        self.csi    = []
+        self.src_mac                 = None
+        self.seq_nr                  = None
+        self.spartial_stream_core_nr = None
+        self.chanspec                = None
+        self.chip_version            = None
+        self.csi_matrix              = None
+        self.csi_matrix_size         = None
 
     @classmethod
     def from_dict(cls, csi_dict):
         """
         init from json
         """
-        csiEntry = cls()
-        csiEntry.code = csi_dict["code"]
-        csiEntry.bfee_count = csi_dict["bfee_count"]
-        csiEntry.Nrx = csi_dict["Nrx"]
-        csiEntry.Ntx = csi_dict["Ntx"]
-        csiEntry.rssiA = csi_dict["rssiA"]
-        csiEntry.rssiB = csi_dict["rssiB"]
-        csiEntry.rssiC = csi_dict["rssiC"]
-        csiEntry.noise = csi_dict["noise"]
-        csiEntry.agc = csi_dict["agc"]
-        csiEntry.antenna_sel = csi_dict["antenna_sel"]
-        csiEntry.length = csi_dict["length"]
-        csiEntry.rate = csi_dict["rate"]
-        csiEntry.rssiA_db = csi_dict["rssiA_db"]
-        csiEntry.rssiB_db = csi_dict["rssiB_db"]
-        csiEntry.rssiC_db = csi_dict["rssiC_db"]
-
-
-
-        csiEntry.csi = csi_dict["csi"]
-        csiEntry.perm = csi_dict["perm"]
-        csiEntry.csi_pwr = csi_dict["csi_pwr"]
-        csiEntry.rssi_pwr = csi_dict["rssi_pwr"]
-        csiEntry.rssi_pwr_db = csi_dict["rssi_pwr_db"]
-        csiEntry.scale = csi_dict["scale"]
-        csiEntry.noise_db = csi_dict["noise_db"]
-        csiEntry.quant_error_pwr = csi_dict["quant_error_pwr"]
-        csiEntry.total_noise_pwr =  csi_dict["total_noise_pwr"]
-        csi_list = csiEntry.csi
-        for rx in range(len(csi_list)):
-            for tx in range(len(csi_list[rx])):
-                for i in range(len(csi_list[rx][tx])):
-                    csi_list[rx][tx][i] = complex(csi_list[rx][tx][i])
-        csiEntry.csi = csi_list
+        csiEntry                            = cls()
+        csiEntry.rx                         = csi_dict.get("rx", None)                    
+        csiEntry.rssiA                      = csi_dict.get("rss", None)                   
+        csiEntry.rssiA                      = csi_dict.get("rssA", None)                  
+        csiEntry.rssiB                      = csi_dict.get("rssB", None)                  
+        csiEntry.rssiC                      = csi_dict.get("rssC", None)                      
+        csiEntry.noise                      = csi_dict.get("noise", None)                     
+        csiEntry.snr                        = csi_dict.get("snr", None)                           
+        csiEntry.csi                        = csi_dict.get("csi", None)                           
+        csiEntry.src_mac                    = csi_dict.get("src_mac", None)                   
+        csiEntry.seq_nr                     = csi_dict.get("seq_nr", None)                        
+        csiEntry.spartial_stream_core_nr    = csi_dict.get("spartial_stream_core_nr", None)       
+        csiEntry.chanspec                   = csi_dict.get("chanspec", None)                  
+        csiEntry.chip_version               = csi_dict.get("chip_version", None)              
+        csiEntry.csi_matrix                 = csi_dict.get("csi_matrix", None)                
+        csiEntry.csi_matrix_size            = csi_dict.get("csi_matrix_size", None)           
         return csiEntry
-    def __str__(self):
-        myString = "CSI Entry:\n"
-        myString += "\t Correct: " + str(self.correct) + "\n"
-        if not self.correct:
-            return myString
 
-        myString += "\t Code: " + str(self.code) + "\n"
-        myString += "\t bfee_count: " + str(self.bfee_count) + "\n"
-        myString += "\t Ntx: " + str(self.Ntx) + "\n"
-        myString += "\t Nrx: " + str(self.Nrx) + "\n"
-        myString += "\t MCS Rate: " + str(self.rate) + "\n"
-        myString += "\t Rssi A [dB]: " + str(self.rssiA_db) + "\n"
-        myString += "\t Rssi B [dB]: " + str(self.rssiB_db) + "\n"
-        myString += "\t Rssi C [dB]: " + str(self.rssiC_db) + "\n"
-        myString += "\t Total Rssi [dB]: " + str(np.round(self.rssi_pwr_db,2)) + "\n"
-        myString += "\t Agc: " + str(self.agc) + "\n"
-        myString += "\t Antenna Sel: " + str(self.antenna_sel) + "\n"
-        myString += "\t Thermal Noise [dB]: " + str(self.noise_db) + "\n"
-        myString += "\t Quantization Noise [dB]: " + str(np.round(self.quant_error_pwr,2)) + "\n"
-        myString += "\t Total Noise [dB]: " + str(np.round(self.total_noise_pwr,2)) + "\n"
-        myString += "\t Permutation vector: " + str(self.perm) + "\n"
-        myString += "\t CSI matrix: " + str(self.csi) + "\n"
-        return myString
 
     def _to_json(self):
         def default(prop):
@@ -114,17 +65,13 @@ class CsiEntry( json.JSONDecoder):
 
         csi_list = self.csi.tolist()
 
-        for rx in range(len(csi_list)):
-            for tx in range(len(csi_list[rx])):
-                for i in range(len(csi_list[rx][tx])):
-                    csi =csi_list[rx][tx][i]
-                    csi_list[rx][tx][i] = str(csi)
+
+        for i in range(len(csi_list)):
+            csi =csi_list[i]
+            csi_list[i] = str(csi)
         self.csi = csi_list
         json_str = json.dumps(self,default=default, indent=True)
-        #json_str= json.dumps(self, default=lambda o: o.__dict__, 
-        #    sort_keys=True, indent=4)
         self.csi = csi_save
-        #json_str= (json.dumps(csi_list))
         return json_str
     
     def to_dict(self):
@@ -132,10 +79,9 @@ class CsiEntry( json.JSONDecoder):
     
 
 def plot_ampli(csi_entry : CsiEntry):
-    for tx in range(csi_entry.Ntx):
-        for rx in range(csi_entry.Nrx): 
-            amplitudes = [abs(csi_comp) for csi_comp in csi_entry.csi[tx][rx]]
-            plt.plot(range(len(csi_entry.csi[tx][rx])), amplitudes, label="Antenna {}->{}".format(tx,rx))
-        break
+
+    amplitudes = [abs(csi_comp) for csi_comp in csi_entry.csi]
+    plt.plot(range(len(csi_entry.csi)), amplitudes, label="Antenna {}".format(csi_entry.rx))
+    
     plt.xlabel('Subcarrier index')
     plt.ylabel('amplitude')
